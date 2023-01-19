5258115873186eae95868c629ec3f07defdbf11c
DanielArruda98 <arruda.daniel20@gmail.com>
2023-01-19 20:11:17 -0300

    aula 02 - backend

---
+++ server/prisma/migrations/20230118222424_add_unique_habit_week_days/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[habit_id,week_day]` on the table `habit_week_days` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,14 @@
 import Fastify from 'fastify'
-import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
-app.register(cors);
-
-app.get('/', () => {
-    return prisma.habit.findMany();
-})
+app.register(cors)
+app.register(appRoutes)
 
 app.listen({
     port: 3333
 }).then(() => {
-    console.log('HTTP server start!');
+    console.log('HTTP server start!')
 })
